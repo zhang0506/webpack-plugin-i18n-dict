d2cfb80ebbb7836c129bded390273f7f00ff6517
zhangxiaoru1 <zhangxiaoru1@jd.com>
2022-04-08 10:32:15 +0800

    优化逻辑

--- packages/dictionary-plugin/src/index.js
+++ packages/dictionary-plugin/src/index.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const path = require('path')
 let file = ''
 let NODE_ENV = 'production'
+var arrDict = []
 
 class DictionaryPlugin {
   constructor(callBack) {
@@ -12,7 +13,7 @@ class DictionaryPlugin {
   apply(compiler) {
     compiler.plugin('done', stats => {
       // 在 done 事件中回调 doneCallback
-      const filePath = path.join(compiler.outputPath, 'js')
+      const filePath = compiler.outputPath
       file = path.join(compiler.outputPath, 'dictData.json')
       if (process.env.NODE_ENV === NODE_ENV) {
         fileDisplay(filePath)
@@ -23,23 +24,34 @@ class DictionaryPlugin {
     })
   }
 }
+
+function dedupe(arr) {
+  var arr1 = [] // 新建一个数组来存放arr中的值
+  for (var i = 0, len = arr.length; i < len; i++) {
+    if (arr1.indexOf(arr[i]) === -1) {
+      arr1.push(arr[i])
+    }
+  }
+  return arr1
+}
 /**
  * 读取原有文件方法
  * @param arr 需要写入的内容
  */
 function createFile(arr) {
-  if (fs.existsSync(file)) {
-    fs.readFile(file, 'utf8', (err, data) => {
-      if (err) {
-        console.error(err)
-      } else {
-        const a = JSON.parse(data)
-        writeF(a.concat(arr))
-      }
-    })
-  } else {
-    writeF(arr)
-  }
+  // if (fs.existsSync(file)) {
+  //   fs.readFile(file, 'utf8', (err, data) => {
+  //     if (err) {
+  //       console.error(err)
+  //     } else {
+  //       const a = JSON.parse(data)
+  //       writeF(dedupe(a.concat(arr)))
+  //     }
+  //   })
+  // } else {
+  //   writeF(arr)
+  // }
+  writeF(dedupe(arr))
 }
 /**
  * 文件写入方法
@@ -76,12 +88,20 @@ function fileDisplay(filePath) {
           } else {
             const isFile = stats.isFile() // 是文件
             const isDir = stats.isDirectory() // 是文件夹
-            if (isFile) {
+            if (isFile && filedir.indexOf('.js') != -1) {
               // console.log(filedir)
               const dictKey = fs
                 .readFileSync(filedir, 'utf8')
-                .match(/(?<=\$t\(("|'))(.*?)(?=("|')\))/g)
-              if (dictKey != null) createFile(dictKey)
+                .match(/(?<=(\.|\$)t\(("|'))(.*?)(?=("|')\))/g)
+              if (dictKey != null) {
+                dictKey.forEach(item => {
+                  if (item.indexOf('(') === -1 && item.indexOf(')') === -1) {
+                    console.log(item)
+                    arrDict.push(item)
+                  }
+                })
+                createFile(arrDict)
+              }
             }
             if (isDir) {
               fileDisplay(filedir) // 递归，如果是文件夹，就继续遍历该文件夹下面的文件
--- packages/internationlization/src/utils/main.js
+++ packages/internationlization/src/utils/main.js
@@ -8,54 +8,78 @@ let i18n = {}
 // 切换语言方法
 function internationlization(language) {
   languageName = language
-  indexDB.openDatabase(appName).then(res => {
-    indexDB.read(languageName, res).then(e => {
-      console.log(res)
-      if (e) {
-        console.log('获取到了数据', e)
-        battleVersion(e.version, res, e)
-      } else {
-        getNewDict(res, 'add')
-      }
+  localStorage.setItem('internationlization', languageName)
+  return new Promise((resolve, reject) => {
+    indexDB.openDatabase(appName).then(res => {
+      indexDB.read(languageName, res).then(e => {
+        if (e) {
+          console.log('获取到了数据', e)
+          battleVersion(e.version, res, e).then(() => {
+            resolve()
+          }).catch((err) => {
+            reject(err)
+          })
+        } else {
+          getNewDict(res, 'add').then(() => {
+            resolve()
+          }).catch((err) => {
+            reject(err)
+          })
+        }
+      }).catch((err) => {
+        reject(err)
+      })
+    }).catch((err) => {
+      reject(err)
     })
   })
 }
 // 对比当前的版本是否是最新的
 function battleVersion(version, _db, e) {
-  axios.get(`/api/i18n/common/lang/version?group=${appName}&langCode=${languageName}`).then(res => {
-    if (version === res.data.data) {
-      changLanguage(e.dict)
-    } else {
-      // 从后端获取新的字典文件
-      getNewDict(_db, 'put')
-    }
+  return new Promise((resolve, reject) => {
+    axios.get(`/i18n/common/lang/version?group=${appName}&langCode=${languageName}`).then(res => {
+      if (version === res.data) {
+        changLanguage(e.dict)
+        resolve()
+      } else {
+        // 从后端获取新的字典文件
+        getNewDict(_db, 'put').then(() => {
+          resolve()
+        })
+      }
+    }).catch((err) => {
+      reject(err)
+    })
   })
 }
 // 从后端获取新的字典文件
 function getNewDict(_db, type) {
-  axios
-    .get(`/api/i18n/common/lang/groupDict?group=${appName}&langCode=${languageName}`)
-    .then(res => {
-      if (type === 'add') {
-        indexDB.add(
-          { language: languageName, version: res.data.data.version, dict: res.data.data.dict },
-          _db
-        )
-      } else {
-        indexDB.put(
-          { language: languageName, version: res.data.data.version, dict: res.data.data.dict },
-          _db
-        )
-      }
-
-      changLanguage(res.data.data.dict)
-    })
+  return new Promise((resolve, reject) => {
+    axios
+      .get(`/i18n/common/lang/groupDict?group=${appName}&langCode=${languageName}`)
+      .then(res => {
+        if (type === 'add') {
+          indexDB.add(
+            { language: languageName, version: res.data.version, dict: res.data.dict },
+            _db
+          )
+        } else {
+          indexDB.put(
+            { language: languageName, version: res.data.version, dict: res.data.dict },
+            _db
+          )
+        }
+        changLanguage(res.data.dict)
+        resolve()
+      }).catch((err) => {
+        reject(err)
+      })
+  })
 }
 // 切换语言
 function changLanguage(dict) {
   i18n.setLocaleMessage(languageName, dict)
   i18n.locale = languageName
-  // localStorage.setItem(`internationlization-${appName}`, JSON.stringify({ langCode: languageName }))
 }
 // 引入i18n
 function digisI18n(Vue, app) {
@@ -67,32 +91,34 @@ function digisI18n(Vue, app) {
       'zh-CN': { common: { 登录: '登录' } }
     }
   })
-  // creatFunc()
-  layoutCreatLanguage()
+  // layoutCreatLanguage()
   return i18n
 }
 // 初始化，恢复到上次选择的语言
-// function creatFunc() {
-//   setTimeout(() => {
-//     if (localStorage.getItem(`internationlization-${appName}`)) {
-//       const lanData = JSON.parse(localStorage.getItem(`internationlization-${appName}`))
-//       internationlization(lanData.langCode)
-//     } else {
-//       localStorage.setItem(`internationlization-${appName}`, JSON.stringify({ langCode: 'zh' }))
-//       internationlization('zh')
-//     }
-//   }, 10)
-// }
 function layoutCreatLanguage() {
-  if (localStorage.getItem('internationlization')) {
-    internationlization(localStorage.getItem('internationlization'))
-  } else {
-    internationlization(languageName)
-  }
+  return new Promise((resolve, reject) => {
+    if (localStorage.getItem('internationlization')) {
+      internationlization(localStorage.getItem('internationlization')).then(() => {
+        resolve()
+      }).catch((err) => {
+        reject(err)
+      })
+    } else {
+      internationlization(languageName).then(() => {
+        resolve()
+      }).catch((err) => {
+        reject(err)
+      })
+    }
+  })
+}
+function getLanguage() {
+  return localStorage.getItem('internationlization') || 'zh'
 }
 export default {
   internationlization: internationlization,
   digisI18n: digisI18n,
   changLanguage: changLanguage,
-  layoutCreatLanguage: layoutCreatLanguage
+  layoutCreatLanguage: layoutCreatLanguage,
+  getLanguage: getLanguage
 }
